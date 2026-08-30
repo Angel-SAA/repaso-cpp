3162bbd8c977ed935996d5cd38cedf8eb3595ed9
Angel-SAA <alcantarasa2007@gmail.com>
2026-08-30 15:38:56 -0600

    R8: Intercambiar en tres versiones

---
+++ r08-punteros/ejercicio.cpp
@@ -0,0 +1,15 @@
+void intercambiar1(int a, int b) {
+    int t = a; a = b; b = t;           // no cambia nada afuera
+}
+void intercambiar2(int* a, int* b) {
+    int t = *a; *a = *b; *b = t;       // se llama: (&x, &y)
+}
+void intercambiar3(int& a, int& b) {
+    int t = a; a = b; b = t;           // se llama: (x, y)
+}
+
+int main(){
+    int x = 6;
+    int y = 10;
+    intercambiar1(x, y);
+}
